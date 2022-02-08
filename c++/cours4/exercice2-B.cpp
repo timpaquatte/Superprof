1f789ed6949befc1e317d25d608f3940100dca1c
timpaquatte <timothee.paquatte@polytechnique.edu>
2022-02-08 20:25:53 +0100

    premiere version

--- c++/cours4/exercice2-B.cpp
+++ c++/cours4/exercice2-B.cpp
@@ -9,55 +9,44 @@ mutex m;
 
 int fibonacci(int n)
 {
-    if (n==0)
-    {
+    if (n <= 0)
         return 0;
-    }
-    
-    if (n == 1)
-    {
-        return 1;
-    }
-    
-    return fibonacci(n-1) + fibonacci(n-2);
-    
 
+    if (n == 1)
+        return 1;
+
+    return fibonacci(n-1) + fibonacci(n-2);
 }
 
 
 void task_1(int n)
-{   std::cout<< var<<std::endl;
+{
     m.lock();
-    var = var+ fibonacci(n-1);
+    var = var + fibonacci(n-1);
     m.unlock();
 }
 
 void task_2(int n)
-{   
-    std::cout<< var<<std::endl;
+{
     m.lock();
     var = var + fibonacci(n-2);
     m.unlock();
-    
 }
 
 
 int fibo(int n)
 {
-    std::cout<< "fib"<<std::endl;
     std::thread t_n_1 (task_1,n);
     std::thread t_n_2(task_2, n);
     t_n_1.join();
     t_n_2.join();
-    
+
     return var;
-    
-   
 }
 
 
 int main(int argc, char *argv[])
 {
-    std::cout<< fibo(12)<<std::endl;
+    std::cout<< fibo(atoi(argv[1]))<<std::endl;
     return 0;
 }
