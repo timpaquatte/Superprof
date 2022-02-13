7c6e0fed89a5d397c2b5d512372de8620f55b807
Bat El <battestielodie22@gmail.com>
2022-02-13 20:20:09 +0100

    pb à resoudre: les termes calculés sont faux

---
+++ c++/cours5/exo.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <map>
+using namespace std;
+
+static int var =0;
+mutex m;
+map<int, int> mapValues;
+
+
+void fibonacci(int n, int* returnValue,int* nbThreads)
+{
+    if (n <= 0) {
+        *returnValue = 0;
+        return;
+    }
+
+    if (n == 1) {
+        *returnValue = 1;
+        return;
+    }
+
+    if(mapValues.find(n) != mapValues.end()) 
+    {
+        *returnValue = mapValues[n];
+        return;
+    }
+    else 
+    {   
+        if(*nbThreads > 1)
+        {
+            int a, b;
+            std::thread t1(fibonacci, n-1, &a,nbThreads);
+            std::thread t2(fibonacci, n-2, &b,nbThreads);
+            *nbThreads = *nbThreads - 2;
+            t1.join();
+            t2.join();
+            *returnValue = a + b;
+            mapValues[n] = a + b;
+
+        }
+
+        else
+
+        {   
+            int a, b;
+            fibonacci(n-1,&a,nbThreads);
+            fibonacci(n-1,&b, nbThreads);
+            *returnValue = a + b;
+
+        }
+        
+
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    int cpt = 8;
+    fibonacci(atoi(argv[1]), &i, &cpt);
+
+    std::cout << i << std::endl;
+    return 0;
+}
